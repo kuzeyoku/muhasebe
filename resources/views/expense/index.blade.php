1d46dc1d6d3a840679295d0e4524aed851eaca5d
Kuzey Oku Software <171201010@ogrenci.karatekin.edu.tr>
2024-11-25 12:18:48 +0300

    Update index.blade.php

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -5,53 +5,56 @@
     </button>
 @endsection
 @section("content")
-    <table class="table table-striped border">
-        <thead class="table-light">
-        <tr>
-            <th>Firma Ünvanı</th>
-            <th>Ruhsat Numarası</th>
-            <th>Harcama Tipi</th>
-            <th>Harcama Tutarı</th>
-            <th>Harcama Tarihi</th>
-            <th class="text-end">İşlemler</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($expenses as $expense)
+    <div class="table-responsive">
+        <table class="table table-striped border">
+            <thead class="table-light">
             <tr>
-                <td>
-                    {{$expense->company?->name}}
-                </td>
-                <td>
-                    {{$expense->licence?->number }}
-                </td>
-                <td>
-                    {{\App\Enums\ExpenseTypeEnum::getLabel($expense->type)}}
-                </td>
-                <td>
-                    {{$expense->amount}}
-                </td>
-                <td>
-                    {{$expense->date}}
-                </td>
-                <td class=" text-end">
-                    <a class="btn btn-sm btn-success" href="{{route("expense.files",$expense)}}">
-                        <i class="las la-file"></i>
-                    </a>
-                    <button type="button" data-url="{{route("expense.edit", $expense)}}"
-                            class="btn btn-sm btn-blue modal-action"><i
-                            class="las la-pen"></i></button>
-                    {{html()->form("DELETE", route("expense.destroy", $expense))->class("d-inline")->open()}}
-                    <button type="button" class="btn btn-sm btn-danger delete-button"><i class="las la-trash-alt"></i>
-                    </button>
-                    {{html()->form()->close()}}
-                </td>
+                <th>Firma Ünvanı</th>
+                <th>Ruhsat Numarası</th>
+                <th>Harcama Tipi</th>
+                <th>Harcama Tutarı</th>
+                <th>Harcama Tarihi</th>
+                <th class="text-end">İşlemler</th>
             </tr>
+            </thead>
+            <tbody>
+            @foreach($expenses as $expense)
+                <tr>
+                    <td>
+                        {{$expense->company?->name}}
+                    </td>
+                    <td>
+                        {{$expense->licence?->number }}
+                    </td>
+                    <td>
+                        {{\App\Enums\ExpenseTypeEnum::getLabel($expense->type)}}
+                    </td>
+                    <td>
+                        {{$expense->amount}}
+                    </td>
+                    <td>
+                        {{$expense->date}}
+                    </td>
+                    <td class=" text-end">
+                        <a class="btn btn-sm btn-success" href="{{route("expense.files",$expense)}}">
+                            <i class="las la-file"></i>
+                        </a>
+                        <button type="button" data-url="{{route("expense.edit", $expense)}}"
+                                class="btn btn-sm btn-blue modal-action"><i
+                                class="las la-pen"></i></button>
+                        {{html()->form("DELETE", route("expense.destroy", $expense))->class("d-inline")->open()}}
+                        <button type="button" class="btn btn-sm btn-danger delete-button"><i
+                                class="las la-trash-alt"></i>
+                        </button>
+                        {{html()->form()->close()}}
+                    </td>
+                </tr>
 
-        @endforeach
-        </tbody>
-    </table>
-    <div class="float-end">
-        {{$expenses->links("pagination::bootstrap-4")}}
+            @endforeach
+            </tbody>
+        </table>
+        <div class="float-end">
+            {{$expenses->links("pagination::bootstrap-4")}}
+        </div>
     </div>
 @endsection
